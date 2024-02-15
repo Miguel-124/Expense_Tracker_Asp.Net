ca52acd1c5ade2dcac09c84f4eb4bb9be33eca4e
Miguel-124 <michalgorecki124@gmail.com>
2024-02-15 16:13:54 +0100

    Changes changes

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Expense_Tracker_Asp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
